9ad94413872417342ee6c3a6ce6fe33b1bc3ae1a
joshua.guillory77@gmail.com <jguillo@DESKTOP-48EART0.localdomain>
2019-10-24 15:37:09 -0500

    add 00_csharp program

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using sc = System.Console; //import with alias 
+
+namespace HelloWorld.Client
+{
+ /// <summary>
+ /// Contains the start of the program
+ /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Starts the Program
+        /// </summary>
+        private static void Main()
+        {
+            sc.WriteLine("Hello World!");
+        }
+    }
+}
